09c9b7767dc5dd18945ecfe953c9929ed0872374
Pedro Conti <pedrohmconti@gmail.com>
2026-09-23 10:26:09 -0300

    Implementando a consulta do log de temperaturas

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/domain/model/TemperatureLog.java
@@ -0,0 +1,32 @@
+package com.algaworks.algasensors.temperature.monitoring.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class TemperatureLog {
+
+    @Id
+    @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "uuid"))
+    private TemperatureLogId id;
+
+    @Column(name = "temp_value")
+    private Double value;
+
+    private OffsetDateTime registeredAt;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "sensor_id", columnDefinition = "BIGINT"))
+    private SensorId sensorId;
+
+}
